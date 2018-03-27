f83d1ef5c85cc1ca14878aaa5d6e2750b20f22f5
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-03-27 19:35:13 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,6 +1,11 @@
 class Project
-  attr_reader :title
+  attr_reader :title, :backers
+  @backers = []
   def initialize(title)
     @title = title
+  end
+
+  def add_backer(backer)
+    @backers << backer
   end
 end
